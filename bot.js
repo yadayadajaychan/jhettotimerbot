14af34aa0c19ca133e42a8de00a476169f743b9a
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-28 12:46:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-       message.reply('pong');
+       message.reply('nigger');
        }
 });
 
