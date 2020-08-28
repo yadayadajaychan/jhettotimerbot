88550969ae8443307cd754bafdd944c563652250
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-28 12:46:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -13,4 +13,4 @@ client.on('message', message => {
        }
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
