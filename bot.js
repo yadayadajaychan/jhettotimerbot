f55167b51b839b70df83d82713b6492c58d8c5d3
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-28 11:48:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,10 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
-
 client.on('ready', () => {
 
     console.log('I am ready!');
 
 });
-
- 
-
 client.on('message', message => {
 
     if (message.content === 'ping') {
@@ -21,9 +14,6 @@ client.on('message', message => {
        }
 
 });
+// dont change
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
