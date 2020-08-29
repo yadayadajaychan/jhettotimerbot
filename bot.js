34490606ee3448284e3cea15fdda72fc174b2dd8
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-28 18:24:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 client.once('ready', () => {
     console.log('Ready!');
