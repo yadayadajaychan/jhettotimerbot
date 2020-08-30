be4e6370df37fa637dd70a7753365f923d160344
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-30 16:22:44 -0700

    Update timer.js

--- commands/timer.js
+++ commands/timer.js
@@ -4,8 +4,17 @@ module.exports = {
 	execute(message, args) {
 		if (args[1] === 's') {
 			unit = "second(s)";
+		} else if (args[1] === 'm') {
+			unit = "minute(s)";
+		} else if (args[1] === 'h') {
+			unit = "hour(s)";
+		} else if (args[1] === 'd') {
+			unit = "day(s)";
+		} else if (args[1] === 'y') {
+			unit = "year(s)";
 		} else {
-			unit = "idk";
+			message.channel.send("You made an error nigger");
+			return
 		}
 		message.channel.send(`${args[0]}`);
 		message.channel.send(unit);
