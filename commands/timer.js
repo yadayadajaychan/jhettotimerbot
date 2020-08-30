39c6568d16984f200241c618be29fe486022d115
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-30 16:04:44 -0700

    Update timer.js

--- commands/timer.js
+++ commands/timer.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'timer',
 	description: 'timer',
 	execute(message, args) {
-		message.channel.send(args);
+		message.channel.send(`time: ${args[0]}`);
+		message.channel.send(`unit: ${args[1]}`);
+		message.channel.send(`description(optional): ${args[2]}`);
 	},
 };
