29c130733144a2380827dbb4822046df3749c46f
yadayadajaychan <67482118+yadayadajaychan@users.noreply.github.com>
2020-08-30 17:40:47 -0700

    Update timer.js

--- commands/timer.js
+++ commands/timer.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'timer',
 	execute(message, args) {
 		//checks if arg[0] is NaN
-		var number = parseFloat(args[0]);
+		var number = parseInt(args[0]);
 		if (isNaN(number)) {
 			message.channel.send("ching chong can't do math");
 			return;
@@ -11,14 +11,19 @@ module.exports = {
 		// finds the unit of time
 		if (args[1] === 's') {
 			unit = "second(s)";
+			timer = number;
 		} else if (args[1] === 'm') {
 			unit = "minute(s)";
+			timer = number * 60;
 		} else if (args[1] === 'h') {
 			unit = "hour(s)";
+			timer = number * 3600;
 		} else if (args[1] === 'd') {
 			unit = "day(s)";
+			timer = number * 86400;
 		} else if (args[1] === 'y') {
 			unit = "year(s)";
+			timer = number * 31536000
 		} else {
 			message.channel.send("You made a unit error nigger");
 			return;
